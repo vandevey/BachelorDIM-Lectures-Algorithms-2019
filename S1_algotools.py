bbaa5ed6204d5b4f91594d38458edb7e851629f7
Alben <benoit.alexandre.vision@gmail.com>
2019-09-26 16:07:56 +0200

    baby code prposal for table average computatio, FIXME: find and fix errors

--- S1_algotools.py
+++ S1_algotools.py
@@ -6,7 +6,22 @@ Created on Thu Sep 26 14:17:01 2019
 @author: alben
 """
 
-print('My first python3 trials')
-
-myVariable=0
-print('MyVariable=',myVariable)
+#############################
+# Assignement 1:
+# average of only the positive table values
+#.... this is an ugly code
+# FIXME : find out error cases
+def average_above_zero(tab):
+    pos_val_sum=0
+    nElem=0
+    for id in range(len(tab)):
+        if tab[id] >0:
+            nElem+=1
+            pos_val_sum+=tab[id]
+            lastID=id
+    return pos_val_sum/nElem, lastID           
+    
+#test section
+tab_list=[1,2,3,-4,6,-9]
+test, lastID=average_above_zero(tab_list)
+print('test_sum=',test)
